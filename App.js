88b78bec090693f1e902ef76aed2fe1e5edf6454
Nguyen Dang Minh <83014500+dangminh71220002@users.noreply.github.com>
2022-05-28 00:12:12 +0700

    ok

--- App.js
+++ App.js
@@ -5,18 +5,13 @@ import store from "./components/Redux/store";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Setting, SettingGeneral, HomeScreen, Profile, Playlist, Login, LoadingSongs } from './components/'
-<<<<<<< HEAD
-import Search from './components/Search/Search'
-
-=======
 import NowPlaying from "./components/NowPlaying/index"
 import Sleep from "./components/Sleep/Sleep";
->>>>>>> 5a92d34d89ef6ec8541594763b46a3a9d849389b
 export default function App() {
   const Stack = createStackNavigator()
   return (
     <Provider store={store}>
-      {/* <NavigationContainer>
+      <NavigationContainer>
         <Stack.Navigator initialRouteName="LoadingSongs" screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Setting" component={Setting} />
           <Stack.Screen name="SettingGeneral" component={SettingGeneral} />
@@ -27,9 +22,8 @@ export default function App() {
           <Stack.Screen name="LoadingSongs" component={LoadingSongs} />
           <Stack.Screen name="Sleep" component={Sleep} />
         </Stack.Navigator>
-          
-      </NavigationContainer> */}
-      <Search/>
+
+      </NavigationContainer>
     </Provider>
   );
 }
