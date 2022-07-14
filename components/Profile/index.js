54396f4630ec565915360e268cbf5f025b2bb451
minhnhox792 <minhnhox792@gmail.com>
2022-07-15 00:09:45 +0700

    Profile handle logic

--- components/Profile/index.js
+++ components/Profile/index.js
@@ -1,92 +1,164 @@
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { Feather } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { FontAwesome5 } from '@expo/vector-icons';
-import Constants from 'expo-constants';
-import { SafeAreaView, StatusBar, Platform, ScrollView, TouchableOpacity, Image} from 'react-native';
+import { StatusBar, TouchableOpacity, Image } from 'react-native';
 import { LinearGradient } from "expo-linear-gradient";
-import { useNavigation } from '@react-navigation/native';
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-    img :  'https://i1.sndcdn.com/artworks-fJKzeLgbi1zHBOyz-JSsHfw-t500x500.jpg'
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-    img : 'https://i1.sndcdn.com/artworks-fJKzeLgbi1zHBOyz-JSsHfw-t500x500.jpg'
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f68',
-    title: 'Second Item',
-    img : 'https://i1.sndcdn.com/artworks-fJKzeLgbi1zHBOyz-JSsHfw-t500x500.jpg'
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f64',
-    title: 'Second Item',
-    img : 'https://i1.sndcdn.com/artworks-fJKzeLgbi1zHBOyz-JSsHfw-t500x500.jpg'
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f60',
-    title: 'Second Item',
-    img : 'https://i1.sndcdn.com/artworks-fJKzeLgbi1zHBOyz-JSsHfw-t500x500.jpg'
-  },
-];
-const Item = ({title, img, navigation }) => (
-  <View style={styles.item}>
-    <Image style={styles.cdImage} source={{uri: img}}/>
-    <TouchableOpacity onPress={() => navigation.navigate('Playlist')}><Text style={styles.title}>{title}</Text></TouchableOpacity>
-  </View>
-);
-
+import { useEffect, useState } from 'react';
+import { setSongs } from "../Redux/musicSlider";
+import { useDispatch } from "react-redux";
+import { Swipeable, GestureHandlerRootView } from 'react-native-gesture-handler';
+import { AntDesign } from '@expo/vector-icons';
 function Profile({ navigation }) {
-  const renderItem = ({ item }) => <Item title={item.title}  img = {item.img}  navigation={navigation}/>;
+  const dispatch = useDispatch();
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [user, setUser] = useState([]);
+  const [isLoadingUser, setisLoadingUser] = useState(true);
+  const [songsUsers, setsongsUsers] = useState([]);
+  const getMusics = async () => {
+    try {
+      const response = await fetch('https://us-central1-musdio-6ec90.cloudfunctions.net/app/api/music/get');
+      const json = await response.json().then(data => {
+        dispatch(setSongs(data.data));
+        setData(data.data)
+      })
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+  const getUsers = async () => {
+    try {
+      const response = await fetch('https://us-central1-musdio-6ec90.cloudfunctions.net/app/api/user/SaM1QW1nc2XwTIHAY5Cx');
+      const json = await response.json().then(data => {
+        setUser(data.data);
+      })
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setisLoadingUser(false);
+    }
+  }
+  useEffect(() => {
+    if (data.length == 0) {
+      getMusics();
+    }
+    if (data.length == 0) {
+      getUsers();
+    }
+
+
+  }, []);
+
+  useEffect(() => {
+    if (data.length != 0 && user.length != 0) {
+      data.forEach((m) => {
+        user['favoriteMusics'].forEach((n) => {
+          if (n == m['id']) {
+            setsongsUsers(previous => {
+              const newData = [...previous, m]
+              return newData
+            })
+          }
+        });
+      });
+    }
+  }, [data, user]);
+  const renderRightView = (id) => {
+    return (
+      <View style={styles.swipe}>
+        <TouchableOpacity
+          onPress={() => {
+            axios.put('https://us-central1-musdio-6ec90.cloudfunctions.net/app/api/user/delete/favoriteSong/SaM1QW1nc2XwTIHAY5Cx', {
+              musicId: id
+            })
+              .then(response => {
+                setsongsUsers(oldData => {
+                    const newData = oldData.filter(song => {
+                    return song.id != id;
+                  });
+                  return newData
+                })
+              })
+          }
+          }
+        >
+          <View style={styles.ButtonDelete}>
+
+            <AntDesign name="delete" size={27} color="white" />
+
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+  };
+  const Item = ({ id, title, img, single, navigation }) => (
+    <GestureHandlerRootView>
+      <Swipeable
+        renderRightActions={() => renderRightView(id)}
+      >
+        <TouchableOpacity onPress={() => navigation.navigate("NowPlaying", {
+          playID: [id]
+        })}>
+          <View style={styles.item}>
+            <Image style={styles.cdImage} source={{ uri: img }} />
+            <View style={styles.Single}>
+              <Text style={styles.nameSong} numberOfLines={1}>{title}</Text>
+              <Text style={styles.nameSingle} numberOfLines={1}>{single}</Text>
+            </View>
+            
+          </View>
+        </TouchableOpacity>
+      </Swipeable>
+    </GestureHandlerRootView>
+  );
+  const renderItem = ({ item }) => <Item id={item.id} title={item.name} img={item.img} single={item.singer} navigation={navigation} />;
   return (
     <LinearGradient
       colors={["#1565C0", "#000"]}
       end={[0.05, 0.5]}
       style={styles.LinearGradient}
     >
-    <View style={styles.container}>
+      <View style={styles.container}>
         <View style={styles.header}>
-       <TouchableOpacity style={styles.iconHeader} onPress={() => navigation.navigate('Home')}>
-              <Ionicons name= "ios-chevron-back" size={28} color="white" fontWeight = 'bold' />
-         </TouchableOpacity>
-              <Text style={styles.textHeader} >Profile</Text>
+          <TouchableOpacity style={styles.iconHeader} onPress={() => navigation.navigate('Home')}>
+            <Ionicons name="ios-chevron-back" size={28} color="white" fontWeight='bold' />
+          </TouchableOpacity>
+          <Text style={styles.textHeader} >Profile</Text>
         </View>
         <View style={styles.Bottom}>
           <View style={styles.Bar}>
           </View>
-          <View style={styles.Avatar}> 
+          <View style= {styles.Avatar}>
+          <Image
+      style = {{height : '100%', width: '100%', borderRadius: 100}}
+      source={{uri: user.uri}}/>
           </View>
-          <View style={styles.SquareContent}> 
-          <Text style = {styles.Content}>NGUYỄN SANH TÀI</Text>
+          <View style={styles.SquareContent}>
+            <Text style={styles.Content}> {user.username}</Text>
           </View>
-          <View style={styles.SquareContent}> 
-          <Text style = {styles.ContentNickname}>TÀI PRO</Text>
+          <View style={styles.SquareContact}>
+            <Text style={styles.ContentContact} numberOfLines={1}>Email: {user.email}</Text>
           </View>
-          <View style={styles.SquareContact}> 
-          <Text style = {styles.ContentContact}>Contact Me</Text>
+          <View style={styles.SquareContent}>
+            <Text style={styles.Content}>Birthdate: {user.birthdate}</Text>
           </View>
-          <View style={styles.SquareContent}> 
-          <Text style = {styles.ContentNickname}>666 Playlists</Text>
+          <View style={styles.SquareContent}>
+            <Text style={styles.Content}>Sex: {user.gender}</Text>
           </View>
-          <View style={styles.SquareContent}> 
-          <Text style = {styles.Content}>Your Albums</Text>
+          <View style={styles.SquareContent}>
+            <Text style={styles.ContentNickname}>My Playlist</Text>
           </View>
 
-         <FlatList data={DATA} renderItem={renderItem} keyExtractor={item => item.id} />
-          
-        </View> 
+          <FlatList data={songsUsers} renderItem={renderItem} keyExtractor={item => item.id} />
+
+        </View>
 
         <View style={styles.ToolBar}>
 
         </View>
       </View>
-     
+
 
     </LinearGradient>
 
@@ -106,67 +178,67 @@ const styles = StyleSheet.create({
     top: StatusBar.currentHeight,
 
   },
-  header:{
+  header: {
     flex: 1,
-    flexDirection : 'row',
+    flexDirection: 'row',
   },
-  iconHeader:{
+  iconHeader: {
     width: '40%',
     paddingLeft: '5%',
-    paddingTop : '2%',
+    paddingTop: '2%',
   },
-  textHeader:{
+  textHeader: {
     color: 'white',
-    fontWeight:'bold',
-    fontSize : 20,
-    width : '100%',
+    fontWeight: 'bold',
+    fontSize: 20,
+    width: '100%',
     paddingTop: '2%',
     paddingLeft: '3%',
 
   },
-  Bottom:{
-    
+  Bottom: {
+
     flex: 14,
-   // backgroundColor: 'pink',
-    borderBottomColor: 'white', 
-    borderBottomWidth:  0.5
+    // backgroundColor: 'pink',
+    borderBottomColor: 'white',
+    borderBottomWidth: 0.5
 
   },
-  Bar:{
-    paddingTop: 2, 
+  Bar: {
+    paddingTop: 2,
     backgroundColor: 'white'
   },
-  Avatar:{
+  Avatar: {
     marginTop: '1.5%',
-    width : '30%',
-    height : '20%',
-    borderRadius : 100, 
-    backgroundColor: 'red',
+    width: '30%',
+    height: '20%',
+    borderRadius: 100,
+    //backgroundColor: 'red',
     left: '35%',
   },
-  SquareContent:{
+  SquareContent: {
     marginTop: '2.5%',
-//    backgroundColor: 'white',
+    //    backgroundColor: 'white',
     justifyContent: 'center',
-    alignItems :'center',
+    alignItems: 'center',
   },
-  Content:{
-    fontSize: 20, 
-    fontWeight : 'bold',
-    color : 'white',
+  Content: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
   },
-  ContentNickname:{
-    fontSize: 20, 
-    fontWeight : 'bold',
-    color : 'white',
+  ContentNickname: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
     opacity: 0.6,
   },
-  ContentContact:{
-    fontSize: 20, 
-    fontWeight : 'bold',
-    color : 'black',
+  ContentContact: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
   },
-  SquareContact:{
+  SquareContact: {
     marginLeft: '15%',
     marginTop: '5%',
     width: '70%',
@@ -174,9 +246,9 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     backgroundColor: '#C4C4C4',
     justifyContent: 'center',
-    alignItems :'center',
+    alignItems: 'center',
   },
-  Song:{
+  Song: {
     flexDirection: 'row',
     marginTop: '5%',
     width: '80%',
@@ -184,26 +256,26 @@ const styles = StyleSheet.create({
     borderRadius: 32,
     backgroundColor: 'black',
     justifyContent: 'center',
-    alignItems :'center',
+    alignItems: 'center',
     marginLeft: '10%',
 
   },
-  avatarmini:{
-    width : '20%',
-    height : '100%',
-    borderRadius : 100, 
+  avatarmini: {
+    width: '20%',
+    height: '100%',
+    borderRadius: 100,
     backgroundColor: 'blue',
-    marginLeft : '-43%',
+    marginLeft: '-43%',
   },
-  ContentMusic:{
-    marginLeft : '10%',
-    marginTop : '7%',
-    fontSize: 20, 
-    fontWeight : 'bold',
-    color : 'white',
+  ContentMusic: {
+    marginLeft: '10%',
+    marginTop: '7%',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
     opacity: 0.6
   },
-  Opposite:{
+  Opposite: {
     flexDirection: 'column',
   },
   item: {
@@ -225,5 +297,37 @@ const styles = StyleSheet.create({
     width: '20%',
     height: '150%',
     borderRadius: 100,
+  },
+  nameSong: {
+    paddingLeft: '10%',
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 20,
+    flex: 1,
+  },
+  nameSingle: {
+    paddingLeft: '10%',
+    fontWeight: 'bold',
+    color: '#928989',
+    fontSize: 15,
+    opacity: 100,
+    flex: 1,
+  },
+  ButtonDelete: {
+    top: '15%',
+    left: '28%',
+    width: '80%',
+    height: '70%',
+    backgroundColor: 'red',
+    color: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+  swipe: {
+    margin: 0,
+    alignContent: 'center',
+    justifyContent: 'center',
+    width: 90,
   },
 });
