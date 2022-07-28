ee625332cd3ed444cd1fd7afe7947b2d1e65c516
Nguyen Dang Minh <83014500+dangminh71220002@users.noreply.github.com>
2022-07-28 14:03:59 +0700

    Update AboutUs

--- components/About us/AboutUs.js
+++ components/About us/AboutUs.js
@@ -38,9 +38,10 @@
 //     };
 
 import React, { useState, useRef } from 'react'
-import { SafeAreaView, View, ScrollView, Text, Dimensions, TouchableOpacity, Animated, StyleSheet, ToastAndroid, ImageBackground } from 'react-native'
+import { SafeAreaView, View, Image,ScrollView, Text, Dimensions, TouchableOpacity, Animated, StyleSheet, ToastAndroid, ImageBackground } from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
 import { LinearGradient } from "expo-linear-gradient";
+import reactDom from 'react-dom';
 
 
 
@@ -53,35 +54,43 @@ const AboutUs = () => {
       style={styles.LinearGradient}
     >
       <ImageBackground source={{ uri: "https://media.discordapp.net/attachments/977411778671677471/1000027427046694942/unknown.png?width=400&height=701" }} resizeMode="cover" style={styles.image}>
-        <View >
-          <View style={{ flexDirection: 'row', }}>
-            <TouchableOpacity style={styles.iconBack}>
+        <View>
+          <View style={{ flexDirection: 'row' }}>
+            <TouchableOpacity style={{ padding: 30 }}>
               <Ionicons name="ios-chevron-back" size={40} color="white" fontWeight='bold' />
             </TouchableOpacity>
-            <View style={{ paddingTop: '1%', alignItems: 'center', justifyContent: 'center', alignContent: 'center', paddingLeft: '10%' }}>
-              <Text style={{
-                fontSize: 14,
-                fontWeight: 'bold',
-                color: 'white',
-              }}> ABOUT US</Text>
+            <View style={styles.header}>
+              <Text style={{ fontSize: 24, color: 'white', fontWeight: 'bold', marginLeft: '-6%' }}>
+                ABOUT US
+              </Text>
             </View>
           </View>
-
-          <View style={styles.location} >
-            <View style={styles.item}>
-              <Text style={styles.group} > MUSDIO APP</Text>
-            </View>
-          </View>
-          <View style={styles.location} >
-            <View style={styles.item2}>
-              <Text style={styles.group4} >Member</Text>
-              <View style={{ paddingTop: '30%' }}>
-                <Text style={styles.group2} > 1. Trần Tuấn Minh</Text>
+          <View style={styles.display}>
+            <TouchableOpacity>
+              <View style={styles.butom}>
+                <Image style={styles.CDImage} source={{uri: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/290548197_503683528233220_8691048569569969850_n.png?_nc_cat=109&ccb=1-7&_nc_sid=aee45a&_nc_ohc=jNe7MsgwZ3UAX8dPRBe&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVKYZHHIBQQu8n7hYkz8R-jksxclDsU5pt2QnVU8zXAGMw&oe=6305893F'}} />
               </View>
-              <Text style={styles.group2} > 2. Nguyễn Đăng Minh</Text>
-              <Text style={styles.group2} > 3. Nguyễn Phát Thịnh</Text>
-              <Text style={styles.group2} > 4. Nguyễn Sanh Tài</Text>
-            </View>
+              <Text style={{paddingLeft:25,fontSize:18,color:'white',fontWeight:'600'}}> Trần Tuấn Minh </Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View style={styles.butom}>
+              <Image style={styles.CDImage} source={{uri: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/290965560_4886898538082951_5836641614305850718_n.png?_nc_cat=106&ccb=1-7&_nc_sid=aee45a&_nc_ohc=CWzwSxk-zZYAX8exjNV&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVJKOYLFPmurv_QboBB_XZTsCnDyMjQfO6c0gGZQCD-gXg&oe=63040451'}} />
+              </View>
+              <Text style={{paddingLeft:25,fontSize:18,color:'white',fontWeight:'600'}}> Nguyễn Đăng Minh </Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View style={styles.butom}>
+              <Image style={styles.CDImage} source={{uri: 'https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.15752-9/294889061_417514530352329_1718781898845123060_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_ohc=Vy94xle6vTAAX_v5bcu&_nc_ht=scontent.fsgn5-2.fna&oh=03_AVK0exCOW_bgDtwxoiJlhgspTPcjoUe3kINijNsgdRtpSQ&oe=63063145'}} />
+              </View>
+            </TouchableOpacity>
+            
+            <TouchableOpacity>
+              <View style={styles.butom}>
+              <Image style={styles.CDImage} source={{uri: 'https://scontent.fsgn5-13.fna.fbcdn.net/v/t1.15752-9/289873096_724234605676315_7238915175904753965_n.png?stp=cp0_dst-png&_nc_cat=106&ccb=1-7&_nc_sid=ae9488&_nc_ohc=_ShadFODFxgAX_TJMiY&tn=3T8UXKUZPmicUvrP&_nc_ht=scontent.fsgn5-13.fna&oh=03_AVJv284AXrf2bbs06HB9bATVFZ38dJ7STOhuJH-VYBoHqA&oe=6303FCB4'}} />
+              </View>
+            </TouchableOpacity>
+            <Text style={{paddingLeft:25,fontSize:18,color:'white',fontWeight:'600'}}> Nguyễn Phát Thịnh</Text>
+            <Text style={{paddingRight:10,fontSize:18,color:'white',fontWeight:'600'}}> Nguyễn Sanh Tài </Text>
           </View>
         </View>
       </ImageBackground>
@@ -90,63 +99,39 @@ const AboutUs = () => {
 }
 
 const styles = StyleSheet.create({
-  location: {
-    //alignContent: 'center',
-    alignItems: 'center',
-    //justifyContent: 'center',
-    // paddingTop:'30%',
-    //backgroundColor: 'blue',
+  header: {
+    marginTop: 30, alignContent: 'center', alignItems: 'center', justifyContent: 'center', marginLeft: '-8%',
+    width: '80%', height: '30%',marginTop:40,
+  },
+  butom: {
+    backgroundColor: 'red',
+    height:130,
+    width:130,
+    margin:30,
+    marginTop:40
 
   },
-  iconBack: {
-    width: '30%',
-    paddingRight:"10%"
+  display:{
+    display:'flex',
+    flexDirection :"row",
+    flexWrap:'wrap',
+    justifyContent:"space-between",
+
   },
-  group: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    color: 'white',
-    padding: 30,
-    backgroundColor: '#A821FF',
-    borderRadius: 30,
-  },
-  group4: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    color: 'white',
-    padding: 30,
-    backgroundColor: '#A821FF',
-    borderRadius: 30,
-    paddingLeft:'27%',
-  },
-  group2: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  item: {
-    width: '100%',
-    height: '35%',
-    borderRadius: 25,
-    alignContent: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  item2: {
-    width: '100%',
-    height: '65%',
-    borderRadius: 30,
+  CDImage:{
+    height:130,
+    width:130,
   },
   LinearGradient: {
     width: "100%",
-    alignItems: "center",
+    height: '100%',
     justifyContent: "flex-start",
   },
   image: {
     width: "100%",
-    alignItems: "center",
+    height: '100%',
     justifyContent: "flex-start",
-    paddingTop: 40
   },
+
 });
 export default AboutUs
